8d3efd10964d9a58565642e854fb76f21635f354
Jeff Cameron <jeffgcameron@gmail.com>
2021-04-09 13:45:55 -0600

    login/out, profile, dashboard

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const User = require('./User');
+const Project = require('./Project');
+// const Comment = require("./Comments");
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project, };
